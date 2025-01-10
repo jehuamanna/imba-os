5922158d8866a244dfbbf93e60647813ab6f5d49
Jehu Shalom <jehuamanna@gmail.com>
2025-01-10 09:11:21 -0500

    changes

--- files/scripts/steam_lutris_others.sh
+++ files/scripts/steam_lutris_others.sh
@@ -20,31 +20,6 @@ rpm-ostree install \
     cp /usr/lib/dri/swrast_dri.so /tmp/mesa-fix32/dri/ && \
     cp /usr/lib/dri/virtio_gpu_dri.so /tmp/mesa-fix32/dri/ && \
     
-    
-    rpm-ostree override replace \
-    --experimental \
-    --from repo=copr:copr.fedorainfracloud.org:kylegospo:bazzite-multilib \
-        mesa-libxatracker \
-        mesa-libglapi \
-        mesa-dri-drivers \
-        mesa-libgbm \
-        mesa-libEGL \
-        mesa-vulkan-drivers \
-        mesa-libGL \
-        pipewire \
-        pipewire-alsa \
-        pipewire-gstreamer \
-        pipewire-jack-audio-connection-kit \
-        pipewire-jack-audio-connection-kit-libs \
-        pipewire-libs \
-        pipewire-pulseaudio \
-        pipewire-utils \
-        pipewire-plugin-libcamera \
-        bluez \
-        bluez-obexd \
-        bluez-cups \
-        bluez-libs \
-        xorg-x11-server-Xwayland && \
 rsync -a /tmp/mesa-fix64/ /usr/lib64/ && \
 rsync -a /tmp/mesa-fix32/ /usr/lib/ && \
 rm -rf /tmp/mesa-fix64 && \
